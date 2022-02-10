d3ad5a3a0adbba968ebc4c3e42c618ee105acbb7
uriel levy <uriellevy4892@gmail.com>
2022-02-10 14:48:21 +0200

    getcity http request added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomeView from "./components/HomeView/HomeView";
 import Favorites from "./components/Favorites/Favorites.jsx";
+import { WeatherProvider } from "./components/context/WeatherContext.jsx";
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/" exact component={HomeView} />
-        <Route path="/favorites" exact component={Favorites} />
-      </Switch>
-    </Router>
+    <WeatherProvider>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={HomeView} />
+          <Route path="/favorites" exact component={Favorites} />
+        </Switch>
+      </Router>
+    </WeatherProvider>
   );
 }
 
---
+++ src/components/context/WeatherContext.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, createContext } from "react";
+
+export const WeatherContext = createContext();
+
+export const WeatherProvider = (props) => {
+  const apiKey = "w1B0oqUDkLzkIiuxyC6Agc4bolPrw4Gt";
+
+  const getCity = async (cityName) => {
+    const res = await fetch(
+      `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${cityName}`
+    );
+    const data = await res.json();
+
+    return data[0];
+  };
+
+  getCity("munich");
+
+  console.log(getCity("munich"));
+
+  return <WeatherContext.Provider>{props.children}</WeatherContext.Provider>;
+};
